f12ef8ac0aaf154ed0661c77b980f439779ed50f
constantin-lange <constantin.lange@student.hpi.uni-potsdam.de>
2019-06-14 22:05:33 +0200

    fixed bug with hashmap free

--- src/allsubs.test.c
+++ src/allsubs.test.c
@@ -141,7 +141,9 @@ ddata dt = { 0 }, sub = { 0 };
     Free1D(subset);
 
   }/*FOR*/
-  conting_hashmap = NULL;
+  if(minsize >= 2){
+    conting_hashmap = NULL;
+  }
 
   Free1D(zptr);
   FreeDDT(dt, FALSE);
